8415ca6a1431808f8a829348da9d3eae11c734c5
Murilo Martins <murilomartins@outlook.com>
2023-02-25 11:32:35 -0300

    :construction: Routes fix and Caret CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Home from "./routes/Home";
-import Offer from "./routes/Offer";
-import OfferDetail from "./routes/OfferDetail";
+import Offer from "./routes/Home/Offer";
+import OfferDetail from "./routes/Home/OfferDetail";
 import Subscription from "./routes/Subscription";
 
 export default function App() {
