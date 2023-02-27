6664b4df012f24876065bcf3383fadc22a3745fd
Murilo Martins <murilomartins@outlook.com>
2023-02-27 16:48:15 -0300

    :construction: useState hook

--- src/routes/Home/Subscription/index.tsx
+++ src/routes/Home/Subscription/index.tsx
@@ -1,15 +1,26 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import Button from "../../../components/Button";
 
 export default function Subscription() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
   return (
     <main className="dd-container">
       <div className="dd-card">
         <h1 className="dd-card-title">Página de inscrição</h1>
+        <div>
+          <h4>Número de promoções resgatadas: {count}</h4>
+        </div>
       </div>
       <Link to={"/offer-detail"}>
         <Button text="Ver promoção" />
       </Link>
+      <button className="dd-btn" onClick={handleClick}>Resgatar</button>
     </main>
   );
 }
